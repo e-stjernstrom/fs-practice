a3c267aa762243deb901626d1ce813916b1247b7
Erika Stjernstrøm <e.stjernstrom@outlook.com>
2026-07-09 00:19:08 +1000

    first commit

--- part3/index.js
+++ part3/index.js
@@ -89,7 +89,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
