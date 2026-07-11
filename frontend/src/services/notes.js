f9618d4483fbaf61c4e9c6f289daca5634c7e64d
Erika Stjernstrøm <e.stjernstrom@outlook.com>
2026-07-11 18:35:01 +1000

    Added deployment script for render

--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://192.168.0.5:3001/api/notes'
+const baseUrl = 'http://192.168.0.6:3001/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
